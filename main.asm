ed62ecc557d2fafe267a324f8e659fd8a3adaf66
Yorguin Jose Mantilla <yjmantilla@gmail.com>
2019-08-23 18:13:45 -0500

    more modularization in input subroutine

--- main.asm
+++ main.asm
@@ -9,25 +9,29 @@ N_STORE		.blkw 1
 N_LOW		.fill #15
 N_HIGH		.fill #30
 M		.fill #4
-MSG_N           .stringz "\nError: 15 <= N <= 30"
+MSG_ERROR_N     .stringz "\nError: 15 <= N <= 30"
+MSG_ENTER_N	.stringz "\nFirst enter N"
 
-MAIN					
-		jsr INPUT_N
-		br FINISH
+		lea r0 , MSG_ENTER_N
+		puts
+MAIN		jsr INPUT
+		jsr CHECK_N
+		st r4 , N_STORE
+		br INPUT_N_DONE
 
 
-INPUT_N		; push r7 so we dont lose where we came from
-		;jsr PUSH_R7 bad idea
-		;add r6, r6 , #1
-		;str r7, r6 , #0
-		add r1 , r7 , #0
+INPUT		; subroutine, leaves stuff in r4
+		; push r7 so we dont lose where we came from
+		add r1 , r7 , #0 ; since PUSH_R1 pushes R1
 		jsr PUSH_R1_SCOPE 
-INPUT_N_NO_PUSH	lea r0 , MSG_ENTER_N
+
+
+INPUT_NO_PUSH	lea r0 , MSG_ENTER
 		
 		; we dont need to push r7 so as long we
 		; dont go to something that also goes to a subroutine
 
-		MSG_ENTER_N .stringz "\nEnter N:"
+		MSG_ENTER .stringz "\nEnter it:"
 
 		puts
 		
@@ -38,7 +42,7 @@ INPUT_N_NO_PUSH	lea r0 , MSG_ENTER_N
 		and r2 , r2 , x0000
 		and r4 , r4 , x0000
 		
-INPUT_N_I	getc	; gets c in r0
+INPUT_I		getc	; gets c in r0
 		
 		
 		add r1 , r0 , #-10	; check if enter was pressed
@@ -59,17 +63,18 @@ INPUT_N_I	getc	; gets c in r0
 		add r1 , r1 , #-16
 		add r1 , r1 , #-16
 		add r1 , r1 , #-16
-		brn INPUT_N_I
+		brn INPUT_I
 		; we are already sure it is not lower
 		; if it was a number we are now within 0 - 9
 		; substract 9, if positive it was not a number
 		add r1 , r1 , #-9
-		brp INPUT_N_I	
+		brp INPUT_I	
 
 		;convert to number
-		and r0 , r0 , xF	
+		and r0 , r0 , xF
+			
  		
-		; accumulate, check overflow in each step	
+ACCUM		; accumulate, check overflow in each step	
 		add r1 , r4 , #0 ; r1 <- 1 r4
 		brn OVERFLOW
 		add r4 , r4 , r4 ; r4 <- 2 r1
@@ -82,43 +87,40 @@ INPUT_N_I	getc	; gets c in r0
 		brn OVERFLOW		
 		add r4 , r4 , r0 ; r4 <= 10 r1 + r0
 		brn OVERFLOW
-
 		
+		; check overflow for bubblesort-comparisons
+		add r1 , r4 , r4 ; 2 times the number have to be representable
+		brn OVERFLOW
 
-		br INPUT_N_I
+		br INPUT_I
 		
 NEGATIVE	not r2 , r2
-		br INPUT_N_I		
+		br INPUT_I		
 
 OVERFLOW	lea r0 , MSG_OVERFLOW
 		MSG_OVERFLOW .stringz "\nOverflow, try with a lower number."
-
 		puts
-		br INPUT_N_NO_PUSH
-
-NOT_IN_RANGE	lea r0 , MSG_N
+		br INPUT_NO_PUSH
+OVERFLOW_BUBBLE lea r0 , MSG_B_OVERFLOW
+		MSG_B_OVERFLOW .stringz "\nOverflow for comparison, try with a lower number."
 		puts
-		br INPUT_N_NO_PUSH
+		br INPUT_NO_PUSH
+NOT_IN_RANGE	lea r0 , MSG_ERROR_N
+		puts
+		br MAIN
 		
-INPUT_N_READY	; pop r7 to return (pop stack)
+INPUT_READY	; pop r7 to return (pop stack)
 		
-		;jsr POP_R7 bad idea
-		;ldr r7, r6, #0
-		;add r6, r6, #-1
 		jsr POP_R1_SCOPE
 		add r7 , r1 , #0
 		add r0 , r4 , #0
-		st r4 , N_STORE
+		
 
 		ret	; go home boy
 
 YES_ENTER 	; if enter was first char, it will just assume thats a 0
-		
-
-		
+	
 		; check if we need to negate
-		
-		; prepare to negate
 		add r1 , r4 , #0
 		add r2 , r2 , #0
 		brz DONT_NEGATE
@@ -127,8 +129,12 @@ YES_ENTER 	; if enter was first char, it will just assume thats a 0
 		; either way we have the correct stuff in r1
 		; move it back to r4
 DONT_NEGATE	add r4 , r1 , #0
+		br INPUT_READY
 
-		; check range
+CHECK_N		; check range for N
+		; assumes N in r4
+		add r1 , r7 , #0
+		jsr PUSH_R1_SCOPE
 		
 		ld r1 , N_LOW
 		jsr NEGATE_R1
@@ -139,21 +145,26 @@ DONT_NEGATE	add r4 , r1 , #0
 		jsr NEGATE_R1
 		add r1 , r4 , r1
 		brp NOT_IN_RANGE
-
+		
+		jsr POP_R1_SCOPE
+		add r7 , r1 , #0
 		; else we are ready to go
-		br INPUT_N_READY
+		ret
 
 NEGATE_R1	; places in r1 the 2complement of r1
 		not r1 , r1
 		add r1 , r1 , #1
 		ret
 		
-FINISH		lea r0 , MSG_END
-		puts					;
+INPUT_N_DONE	lea r0 , MSG_N_DONE
+		puts
+MENU		lea r0 , MSG_MENU
+		puts
+		MSG_MENU .stringz "\n1 N again\n2 Higher value\n3 Descending Sort\n4 Halt"					;
 		halt	; end program
-		MSG_END .stringz "\nImma halting"
+		MSG_N_DONE .stringz "\nN ok!"
 
-;LIFO STACK
+;LIFO STACK uses r1 as auxiliar register
 PUSH_R1_SCOPE	;r6 is used as stack register
 		add r6, r6, #1
 		str r1, r6, #0
